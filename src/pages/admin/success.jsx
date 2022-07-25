ce10a79c68662d610e705120054b4d76b8a39554
pratamamp <pratamamp@gmail.com>
2022-07-26 06:26:32 +0700

    update success page

--- src/pages/admin/success.jsx
+++ src/pages/admin/success.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Lottie from "react-lottie";
 import { useNavigate } from "react-router-dom";
+
+const API_URL = process.env.REACT_APP_API_GATEWAY;
 
 function Success() {
   const navigation = useNavigate();
@@ -10,24 +12,26 @@ function Success() {
     renderer: "svg",
     animationData: require("../../assets/paymentsuccess.json"),
   };
+  const [loadingDownload, setLoading] = useState(false)
+
 
   return (
     <div className="bg-white h-screen">
       <div className="p-6 md:mx-auto">
         <div className="w-96 mx-auto my-6">
-          <Lottie options={lottieConfig}></Lottie>
+          <Lottie options={lottieConfig} style></Lottie>
         </div>
 
         <div className="text-center">
           <p className="text-gray-600 my-2">
-            An email has been send to your email@domain.com.
-            <br /> Please check for an email from company.
+            Thank you for completing your secure online payment
+            <br /> Your data is being processed and soon ready to download.
           </p>
           <button
             className="rounded bg-blue-600 p-2 text-white font-lato"
             onClick={() => navigation("/admin")}
           >
-            Back to dashboard
+            To Download Page
           </button>
         </div>
       </div>
