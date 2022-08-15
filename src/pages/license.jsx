8be0c833da1e48e31665fc980db69cd80632ceb8
pratamamp <pratamamp@gmail.com>
2022-08-15 14:28:16 +0700

    revert license

--- src/pages/license.jsx
+++ src/pages/license.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
 import axios from "axios";
+import React, { useState } from "react";
 import Certificate from "../components/certificate";
 
 const API_URL = process.env.REACT_APP_API_EXTRACT;
